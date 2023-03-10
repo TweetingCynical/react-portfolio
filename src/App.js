da6b05c3c81a1199a439160c68b7363718331067
Al Turing <jon@exce-ed.com>
2023-03-10 19:30:19 +0000

    Added routes to three core pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Projects from "./pages/Projects";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/projects" element={<Projects />}></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
