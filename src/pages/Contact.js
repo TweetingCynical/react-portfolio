86ceb0bba799c8bf5c4fdeaf54b17420529f8daa
Al Turing <jon@exce-ed.com>
2023-03-14 22:00:27 +0000

    Added Contact links

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,61 @@
+// Import font awesome for bullet point icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faEnvelope,
+  faLinkedin,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <h2>
+        <span className="bg-purple rounded shadow-lg p-2">Contact Me</span>
+      </h2>
+      <br></br>
+      <p>
+        To find out more about my services, collaborations and more, follow your
+        choice from the links below:
+      </p>
+      <br></br>
+      <div>
+        <span className="bg-purple rounded shadow-lg p-1">
+          <FontAwesomeIcon icon={faGithub} />{" "}
+          <a
+            href="https://github.com/TweetingCynical"
+            className="text-light text-decoration-none"
+          >
+            Github
+          </a>
+        </span>
+      </div>
+      <br></br>
+      <div>
+        <span className="bg-purple rounded shadow-lg p-1">
+          <FontAwesomeIcon icon={faLinkedin} />{" "}
+          <a
+            href="https://www.linkedin.com/in/jon-garnett-smith-b18780261/"
+            className="text-light text-decoration-none"
+          >
+            LinkedIn
+          </a>
+        </span>
+      </div>
+      <br></br>
+      <div>
+        <span className="bg-purple rounded shadow-lg p-1">
+          <FontAwesomeIcon icon={faTwitter} />{" "}
+          <a
+            href="https://twitter.com/tweetingcynical"
+            className="text-light text-decoration-none"
+          >
+            Twitter
+          </a>
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
